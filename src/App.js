f1f24082330de02a74669ae441516b60e17ff3ee
vuongvgc <vuongvgc@gmail.com>
2020-08-13 23:00:04 +0700

    hook useState: bind this

--- src/App.js
+++ src/App.js
@@ -2,23 +2,22 @@ import React from 'react';
 import {useState} from 'react';
 import './App.css';
 import Person from './Person/Person';
-function App () {
-  const [statePerson, setStatePerson] = useState({
+class App extends React.Component {
+  state = {
     person:
     [ {name: 'Victor', age: 24},
       {name: 'My My', age: 26},
       {name: 'Cuc Cuc', age: 27},
     ],
     otherState: null
-  })
-  console.log(statePerson);
-  const switchNameHandle = (name) => {
+  };
+  switchNameHandle = (name) => {
     //console.log('Switch Name Click')
     // Solution
     //const person = this.state.person.slice();
     //person[0].name = 'Victor Do'; 
     // person[0].age = '29'
-    setStatePerson ({
+    this.setState({
     // Solution
     //  person: person
     // prolem
@@ -27,27 +26,29 @@ function App () {
       {name: 'My My', age: 26},
       {name: 'Cuc Cuc', age: 27},
     ],
-    otherState: statePerson.otherState
+    otherState: this.state.otherState
     })
   }
-  return(
+  render() {
+    return(
       <div className="App">
         <h1>Hi I'm React</h1>
         <p>I exiter learning React</p>
-        <button onClick={switchNameHandle.bind(this,'Vuong Do')}>Switch Name</button>
+        <button onClick={this.switchNameHandle.bind(this,'Vuong Do')}>Switch Name</button>
         <Person
-        name={statePerson.person[0].name} 
-        age={statePerson.person[0].age}/>
+        name={this.state.person[0].name} 
+        age={this.state.person[0].age}/>
         <Person 
-        name={statePerson.person[1].name} 
-        age={statePerson.person[1].age}
-        click={switchNameHandle}>
+        name={this.state.person[1].name} 
+        age={this.state.person[1].age}
+        click={() => this.switchNameHandle('Vuong Do')}>
           My Hobbies: Read Book
         </Person>
         <Person 
-        name={statePerson.person[2].name} 
-        age={statePerson.person[2].age}/>
+        name={this.state.person[2].name} 
+        age={this.state.person[2].age}/>
       </div>
     )
+  }
   }
 export default App;
