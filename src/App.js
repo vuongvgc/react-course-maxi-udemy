f9ce7e6b040f0c526af99e16232de7054d8ac960
vuongvgc <vuongvgc@gmail.com>
2020-08-13 21:53:38 +0700

    handle event

--- src/App.js
+++ src/App.js
@@ -7,15 +7,36 @@ class App extends React.Component {
     [ {name: 'Victor', age: 24},
       {name: 'My My', age: 26},
       {name: 'Cuc Cuc', age: 27},
+    ],
+    otherState: null
+  }
+  switchNameHandle = () => {
+    //console.log('Switch Name Click')
+    // Solution
+    //const person = this.state.person.slice();
+    //person[0].name = 'Victor Do'; 
+    // person[0].age = '29'
+    this.setState({
+    // Solution
+    //  person: person
+    // prolem
+    person:
+    [ {name: 'Victor Do', age: 29},
+      {name: 'My My', age: 26},
+      {name: 'Cuc Cuc', age: 27},
     ]
+    })
   }
   render() {
     return(
       <div className="App">
         <h1>Hi I'm React</h1>
         <p>I exiter learning React</p>
+        <button onClick={this.switchNameHandle}>Switch Name</button>
         <Person name={this.state.person[0].name} age={this.state.person[0].age}/>
-        <Person name={this.state.person[1].name} age={this.state.person[1].age}>My Hobbies: Read Book</Person>
+        <Person name={this.state.person[1].name} age={this.state.person[1].age}>
+          My Hobbies: Read Book
+        </Person>
         <Person name={this.state.person[2].name} age={this.state.person[2].age}/>
       </div>
     )
