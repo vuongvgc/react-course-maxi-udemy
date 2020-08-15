45b7d60aa9c72061e89906c9a9c95088785b00f8
vuongvgc <vuongvgc@gmail.com>
2020-08-15 21:15:49 +0700

    add inline style

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,15 @@
 import React from 'react';
 const person = (props) => {
+    const style={
+        width: '500px',
+        backgroundColor: '#eee',
+        boxShadow: '4px 4px #000',
+        border: '1px soild #000',
+        padding:'15px',
+        margin: '20px'
+    }
     return (
-        <div>
+        <div style={style}>
             <h1 onClick={props.click}>Hey I'm {props.name} and I'm {props.age} years old</h1>
             <p>{props.children}</p>
             <input value={props.name} onChange={props.changeName} type='text'/>
